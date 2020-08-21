ab3ddaf1ad6685028117eb4b0d847cce31bbf902
Andreas Walther <andreas.walther@lamp-solutions.de>
2020-08-21 10:59:49 +0200

    Remove Debug Code

--- LampSCookiePanel/Controllers/Frontend/CookiePanel.php
+++ LampSCookiePanel/Controllers/Frontend/CookiePanel.php
@@ -5,10 +5,6 @@ class Shopware_Controllers_Frontend_CookiePanel extends \Enlight_Controller_Acti
     public function preDispatch()
     {
         $pluginPath = $this->container->getParameter('swag_controller_example.plugin_dir');
-
-        $config = $this->container->get('shopware.plugin.cached_config_reader')->getByPluginName($this->getName());
-        die($config);
-
         $this->get('template')->addTemplateDir($pluginPath . '/Resources/views/');
     }
 }
